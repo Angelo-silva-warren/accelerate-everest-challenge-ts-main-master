f90b0994583545e5ee72cb004749353355fbb1c5
Angelo-silva-warren <103595466+Angelo-silva-warren@users.noreply.github.com>
2022-05-31 15:02:19 -0300

    finalizado

--- src/domain/user/helper/UserHelper.ts
+++ src/domain/user/helper/UserHelper.ts
@@ -1,39 +1,37 @@
-import { boolean } from 'joi'
-import Iuser from '../types/UserTypes'
+import Iuser from '../types/UserTypes';
 
 class UserHelper {
-
-  static emailCheck (dados: string, Mock: Iuser[]) {
-    const emailMock: Iuser[] = Object.values(Mock)
-    const lista = emailMock.map((item) => item.email)
-    let result : boolean = true
+  static emailCheck(dados: string, Mock: Iuser[]) {
+    const emailMock: Iuser[] = Object.values(Mock);
+    const lista = emailMock.map((item) => item.email);
+    let result: boolean = true;
 
     lista.forEach((lista) => {
       if (lista !== dados) {
-        result = true
+        result = true;
       } else {
-        result =  false
-        return
+        result = false;
+        return;
       }
-    })
-    return result
+    });
+    return result;
   }
 
-  static list (mock: Iuser[]) {
-    const mocks: Iuser[] = Object.values(mock)
-    const listaEmail = mocks.map((item) => item.email)
-    const listaName = mocks.map((item) => item.full_name)
-    const listArray: object[] = []
+  static list(mock: Iuser[]) {
+    const mocks: Iuser[] = Object.values(mock);
+    const listaEmail = mocks.map((item) => item.email);
+    const listaName = mocks.map((item) => item.full_name);
+    const listArray: object[] = [];
 
     for (let i = 0; i < mocks.length; i++) {
       const objeto: object = {
         name: listaName[i],
-        email: listaEmail[i]
-      }
-      listArray.push(objeto)
+        email: listaEmail[i],
+      };
+      listArray.push(objeto);
     }
-    return listArray
+    return listArray;
   }
 }
 
-export default UserHelper
+export default UserHelper;
--- src/domain/user/services/UserCreateService.ts
+++ src/domain/user/services/UserCreateService.ts
@@ -1,20 +1,17 @@
-import UserHelper from '../helper/UserHelper'
-import util from '../../../util/util'
-import Mock from '../mocks/UserMock'
-import Iuser from '../types/UserTypes'
-import UserSchema from '../helper/JoiHelper'
-import { userInfo } from 'os'
+import Mock from '../mocks/UserMock';
+import Iuser from '../types/UserTypes';
+import UserSchema from '../helper/JoiHelper';
 
 class UserService {
-  static async UserCreate (dados: Iuser) {
+  static async UserCreate(dados: Iuser) {
     try {
-      const newUser = await UserSchema.validateAsync(dados)
-      Mock.push(newUser)
-      return { code: 201, msg: {message : "usuario criado"}}
+      const newUser = await UserSchema.validateAsync(dados);
+      Mock.push(newUser);
+      return { code: 201, msg: { message: 'usuario criado' } };
     } catch (error) {
-      return { code: 422, msg : error}
+      return { code: 422, msg: error };
     }
   }
 }
 
-export default UserService
+export default UserService;
