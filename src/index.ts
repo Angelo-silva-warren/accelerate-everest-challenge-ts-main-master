120697c7638bb974dcae2db7394400f755da7f73
Angelo-silva-warren <103595466+Angelo-silva-warren@users.noreply.github.com>
2022-06-13 15:27:25 -0300

    refactor constructor and class UserRouter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { router } from './presentation/Routes'
+import UserRouter from './presentation/Routes'
 
 const app = express()
+const userRouter = new UserRouter()
 
 app.use(express.json())
-app.use(router)
+app.use(userRouter.router)
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000, localhost:3000')
