ed7fb69060bc40a111a4a7a4ee2c3f6330dd83c9
Angelo-silva-warren <103595466+Angelo-silva-warren@users.noreply.github.com>
2022-06-29 18:41:25 -0300

    refactor : adicionando as interfaces no di

--- src/di/index.ts
+++ src/di/index.ts
@@ -2,36 +2,42 @@ import { container } from 'tsyringe'
 import UserHelper from '../domain/user/helper/UserHelper'
 import UserService from '../domain/user/services/UserCreateService'
 import UserList from '../domain/user/services/UserListService'
+import { Iutil } from '../domain/user/interface/util/UtilTypes'
 import ListController from '../presentation/controllers/ListController'
 import UserController from '../presentation/controllers/UsersController'
 import util from '../util/util'
+import { Ihelper } from '../domain/user/interface/domain/helper/HelperTypes'
+import { Iuserlist } from '../domain/user/interface/domain/services/UserListTypes'
+import { IuserCreate } from '../domain/user/interface/domain/services/UserCreateTypes'
+import { Iusercontroller } from '../domain/user/interface/domain/controller/UserControllerTypes'
+import { Ilistcontroller } from '../domain/user/interface/domain/controller/ListControllerTypes'
 
 // Router
-container.registerSingleton<ListController>(
+container.registerSingleton<Ilistcontroller>(
   'ListController',
   ListController
 )
-container.registerSingleton<UserController>(
+container.registerSingleton<Iusercontroller>(
   'UserController',
   UserController
 )
 // UserControoler
-container.registerSingleton<UserService>(
+container.registerSingleton<IuserCreate>(
   'UserService',
   UserService
 )
 // ListController
-container.registerSingleton<UserList>(
+container.registerSingleton<Iuserlist>(
   'UserList',
   UserList
 )
 // UserListService
-container.registerSingleton<UserHelper>(
+container.registerSingleton<Ihelper>(
   'UserHelper',
   UserHelper
 )
 // UserCreatService
-container.registerSingleton<util>(
+container.registerSingleton<Iutil>(
   'Util',
   util
 )
