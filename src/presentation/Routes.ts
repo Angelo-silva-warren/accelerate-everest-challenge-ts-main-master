cfa1d49327177ee286e992f802e0360fac3e6dec
Angelo-silva-warren <103595466+Angelo-silva-warren@users.noreply.github.com>
2022-07-20 17:32:41 -0300

    refactor: add controllerAdapt to Routes

--- src/presentation/Routes.ts
+++ src/presentation/Routes.ts
@@ -4,20 +4,23 @@ import ListController from './controllers/ListController';
 import { inject, injectable } from 'tsyringe';
 import IRoutes from '../interface/RoutesTypes';
 import { MiddlewareType } from '../interface/middleware/MiddlewareTypes';
-
+import { ControllerAdapterType } from '../interface/middleware/IControllerAdapter';
 @injectable()
 export default class UserRouter implements IRoutes {
   router = Router();
   CreateUserController: UserController;
   ListUserController: ListController;
   UserValidation: MiddlewareType;
+  controllerAdapter: ControllerAdapterType;
 
   constructor(
     @inject('UserController') userController: UserController,
     @inject('ListController') userList: ListController,
     @inject('ValidationMiddleware') userValidation: MiddlewareType,
+    @inject('ControllerAdapter') controllerAdapter: ControllerAdapterType,
   ) {
     this.UserValidation = userValidation;
+    this.controllerAdapter = controllerAdapter;
     this.CreateUserController = userController;
     this.ListUserController = userList;
     this.routes();
@@ -27,9 +30,9 @@ export default class UserRouter implements IRoutes {
     this.router.post(
       '/customer',
       this.UserValidation,
-      this.CreateUserController.handle,
+      this.controllerAdapter(this.CreateUserController),
     );
 
-    this.router.get('/user', this.ListUserController.handle);
+    this.router.get('/user', this.controllerAdapter(this.ListUserController));
   }
 }
