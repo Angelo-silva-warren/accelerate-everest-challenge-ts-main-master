4c42cc38ce2523c7f7199bde5d92ec39340e6c3b
Angelo-silva-warren <103595466+Angelo-silva-warren@users.noreply.github.com>
2022-08-11 15:58:27 -0300

    test: made test for userCreateService

--- src/domain/user/services/UserCreateService.test.ts
+++ src/domain/user/services/UserCreateService.test.ts
@@ -1,13 +1,41 @@
+import 'reflect-metadata';
 import UserService from './UserCreateService';
 import Validation from '../helper/UserValidation';
 import UserRepository from '../Repository/UserRepository';
+import IUser from '@interface/UserTypes';
+import container from '@di/index';
 
-const validate =  Validation();
-const repository = new UserRepository();
-const userService = new UserService(repository, validate);
+const userMock: IUser = {} as IUser;
+beforeAll(() => {
+  userMock.cpf = '123.456.789-09';
+  userMock.email = 'test@test.com.br';
+});
 
 describe('UserService', () => {
   describe('UserCreate', () => {
-    it('should   ');
+    const userService = container.resolve(UserService);
+    const spyRepository = jest
+      .spyOn(UserRepository.prototype, 'create')
+      .mockReturnValue(userMock);
+    it('should return new user when every data is correct', () => {
+      const spyValidation = jest
+        .spyOn(Validation.prototype, 'validate')
+        .mockReturnValue();
+
+      expect(userService.userCreate(userMock)).toBe(userMock);
+      expect(spyValidation).toBeCalledWith(userMock.cpf, userMock.email, []);
+      expect(spyRepository).toBeCalledWith(userMock);
+    });
+    it('should throw error when validate failed', async () => {
+      const mockError = new Error('Some Error');
+      jest.spyOn(Validation.prototype, 'validate').mockImplementation(() => {
+        throw mockError;
+      });
+      try {
+        await userService.userCreate(userMock);
+      } catch (error) {
+        expect(error).toBe(mockError);
+      }
+    });
   });
 });
