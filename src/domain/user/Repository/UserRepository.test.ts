746b03a584ce4ffbb00eb3ca4e05f0dc3ff1e6ac
Angelo-silva-warren <103595466+Angelo-silva-warren@users.noreply.github.com>
2022-08-11 15:54:37 -0300

    refactor: resume mock user

--- src/domain/user/Repository/UserRepository.test.ts
+++ src/domain/user/Repository/UserRepository.test.ts
@@ -11,21 +11,7 @@ describe('UserRepository', () => {
     });
     describe('create', () => {
       it('Should return entity when is call', () => {
-        const entity: IUser = {
-          full_name: 'angelo',
-          email: 'test@gmail.com.br',
-          email_confirmation: 'test@gmail.com.br',
-          cpf: '08559032959',
-          cellphone: '47 988066432',
-          birthdate: '03/09/2002',
-          email_sms: true,
-          whatsapp: true,
-          country: 'brasil',
-          city: 'blumenau',
-          postal_code: '12345678',
-          address: 'rua tal',
-          number: 3,
-        };
+        const entity: IUser = {} as IUser;
         const result = repository.create(entity);
         expect(result).toBe(entity);
       });
