9f1f1184bd71871922dfc4f950324b4589d3cfe2
Angelo-silva-warren <103595466+Angelo-silva-warren@users.noreply.github.com>
2022-08-11 16:00:59 -0300

    refactor: delete useless code

--- src/domain/user/services/UserListService.ts
+++ src/domain/user/services/UserListService.ts
@@ -1,4 +1,3 @@
-import IHelper from '@interface/domain/helper/HelperTypes';
 import IUserRepository from '@interface/domain/Repository/RepositoryTypes';
 import IUserlist from '@interface/domain/services/UserListTypes';
 import IUser from '@interface/UserTypes';
@@ -6,13 +5,8 @@ import { inject, injectable } from 'tsyringe';
 
 @injectable()
 export default class UserList implements IUserlist {
-  userHelper: IHelper;
   userRepository: IUserRepository;
-  constructor(
-    @inject('UserHelper') userHelper: IHelper,
-    @inject('UserRepository') userRepository: IUserRepository,
-  ) {
-    this.userHelper = userHelper;
+  constructor(@inject('UserRepository') userRepository: IUserRepository) {
     this.userRepository = userRepository;
   }
 
